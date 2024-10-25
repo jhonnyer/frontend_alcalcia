ad06367a18f3e0e9b765279b5f6528fb6b30fffd
José Luis Zúñiga <jose@tangibledesign.co>
2024-10-25 12:23:43 -0500

    Add shared common components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,11 @@ export const routes: Routes = [
     component: MainLayoutComponent,
     children: [
       {
+        path: '',
+        redirectTo: 'inventory',
+        pathMatch: 'full'
+      },
+      {
         path: 'inventory',
         loadChildren: () => import('./features/inventory/inventory.routes').then(m => m.INVENTORY_ROUTES)
       },
--- src/app/layouts/components/main-layout/main-layout.component.ts
+++ src/app/layouts/components/main-layout/main-layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { SidebarComponent } from '../../../shared/components/sidebar/sidebar.component';
+import { LoaderComponent } from '../../../shared/components/loader/loader.component';
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, LoaderComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })

0ff360b318dc58e45701f356ab704f9f922f780e
José Luis Zúñiga <jose@tangibledesign.co>
2024-11-08 16:48:02 -0500

    Move path ubication component nucleos

--- src/app/features/nucleo/nucleo.routes.ts
+++ src/app/features/nucleo/nucleo.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from "@angular/router";
-import { NucleosComponent } from "./pages/nucleo-register/nucleos/nucleos.component";
+import { NucleosComponent } from "./pages/nucleos/nucleos.component";
 import { NucleoRegisterComponent } from "./pages/nucleo-register/nucleo-register.component";
 
 export const NUCLEO_ROUTES: Routes = [

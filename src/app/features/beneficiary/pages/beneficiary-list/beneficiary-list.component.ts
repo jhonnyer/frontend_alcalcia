a24da239c8ae52daf7ce07fe61541439813e5d25
José Luis Zúñiga <jose@tangibledesign.co>
2024-11-13 16:37:33 -0500

    Add dinamic table in beneficiary

--- src/app/features/beneficiary/pages/beneficiary-list/beneficiary-list.component.ts
+++ src/app/features/beneficiary/pages/beneficiary-list/beneficiary-list.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { CdkTableModule } from '@angular/cdk/table';
 import { Beneficiary } from '../../interfaces/beneficiary.model';
 import { beneficiaryList } from '../../../../core/data/beneficiary.data';
+import { TableTemplateComponent } from '../../../../shared/components/table-template/table-template.component';
 
 const ELEMENT_DATA: Beneficiary[] = beneficiaryList;
 
@@ -10,13 +11,13 @@ const ELEMENT_DATA: Beneficiary[] = beneficiaryList;
   selector: 'app-beneficiary-list',
   standalone: true,
   imports: [
-    CommonModule, CommonModule, CdkTableModule
+    CommonModule, TableTemplateComponent
   ],
   templateUrl: './beneficiary-list.component.html',
   styles: ``,
 })
 export class BeneficiaryListComponent {
-  displayedColumns: string[] = [
+  displayedColumns: (keyof Beneficiary | 'controls')[] = [
     'id_beneficiario',
     'nombres_apellidos',
     'documento_identidad',
@@ -30,7 +31,27 @@ export class BeneficiaryListComponent {
     'observaciones',
     'controls'
   ]
-  dataSource = ELEMENT_DATA;
+
+  data: Beneficiary[] = ELEMENT_DATA;
+
+  columnSearch = 'categoria';
+
+  sorteablesColumns: string[] = [
+    "codigo",
+    "descripcion",
+    "categoria",
+    "unidad_medida",
+    "proveedor",
+    "fecha_ingreso",
+    "precio_unitario",
+    "stock_actual",
+    "stock_minimo",
+    "ubicacion"
+  ]
+
+  stickyColumns = [
+    "codigo"
+  ]
 
   delete(item: Beneficiary){
     console.log("Eliminar: ", item)
@@ -39,5 +60,4 @@ export class BeneficiaryListComponent {
   update(item: Beneficiary){
     console.log("update: ", item)
   }
-
 }

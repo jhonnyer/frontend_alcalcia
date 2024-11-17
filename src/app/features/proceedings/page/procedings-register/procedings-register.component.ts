e0086e8dc298f1f590f1c203e938e007d5f9ad79
José Luis Zúñiga <jose@tangibledesign.co>
2024-11-17 12:52:37 -0500

    Add component procedings-create

---
+++ src/app/features/proceedings/page/procedings-register/procedings-register.component.ts
@@ -0,0 +1,84 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormArray, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-procedings-register',
+  standalone: true,
+  imports: [
+    CommonModule, ReactiveFormsModule
+  ],
+  styles: ``,
+  templateUrl: './procedings-register.component.html'
+})
+export class ProcedingsRegisterComponent {
+  public formFamilyCore: FormGroup = new FormGroup({});
+  private fb = inject(FormBuilder);
+
+  ngOnInit(): void {
+    this.initFormFamilyCore();
+
+  }
+
+  initFormFamilyCore(): void {
+    this.formFamilyCore = this.fb.group({
+      proyecto: ['', [Validators.required]],
+      responsableVisita: ['', [Validators.required]],
+      direccion: ['', [Validators.required]],
+      prioridad: ['', [Validators.required]],
+      beneficiarios: this.fb.array([])
+    });
+    this.addBeneficiary();
+  }
+
+  initFormBeneficiary(): FormGroup {
+    // Retorna el formulario que estará anidado
+    return this.fb.group({
+      nombre1: ['', [Validators.required]],
+      nombre2: ['', [Validators.required]],
+      apellido1: ['', [Validators.required]],
+      apellido2: ['', [Validators.required]],
+      tipoDocumento: ['', [Validators.required]],
+      numeroDocumento: ['', [Validators.required]],
+      sexo: ['', [Validators.required]],
+      genero: ['', [Validators.required]],
+      victimaConflico: ['', [Validators.required]],
+      fechaNacimiento: ['', [Validators.required]],
+      edad: ['', [Validators.required]],
+      etnia: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      telefono: ['', [Validators.required]],
+    });
+  }
+
+  //Agrega un nuevo formulario anidado de Persona
+  addBeneficiary(): void {
+    const refBeneficiary = this.formFamilyCore.get('beneficiarios') as FormArray;
+    refBeneficiary.push(this.initFormBeneficiary());
+  }
+
+  // Referencia cada campo que se crea en el html
+  getCtrl(key: string, form: FormGroup): FormArray {
+    return form.get(key) as FormArray;
+  }
+
+  get beneficiariosFormArray(): FormArray {
+    return this.formFamilyCore.get('beneficiarios') as FormArray;
+  }
+
+  deletePerson(index: number): void {
+    const beneficiariosArray = this.formFamilyCore.get('beneficiarios') as FormArray;
+    beneficiariosArray.removeAt(index);
+  }
+
+
+  onSubmit() {
+    if(this.formFamilyCore.valid){
+      console.log("Form Family Core");
+      console.log(this.formFamilyCore.value);
+	  }else{
+		  this.formFamilyCore.markAllAsTouched();
+	  }
+  }
+}
--- src/app/features/proceedings/procedings.routes.ts
+++ src/app/features/proceedings/procedings.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from "@angular/router";
 import { ProcedingsListComponent } from "./page/procedings-list/procedings-list.component";
+import { ProcedingsRegisterComponent } from "./page/procedings-register/procedings-register.component";
 
 export const PROCEDINGS_ROUTES: Routes = [
   {
     path: '',
     component: ProcedingsListComponent
-  }
+  },
+  {
+    path: 'register',
+    component: ProcedingsRegisterComponent
+  },
 ]

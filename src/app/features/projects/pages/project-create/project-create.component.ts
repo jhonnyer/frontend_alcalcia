df3d36008a96bebdb27e7875c2092373322183ff
José Luis Zúñiga <jose@tangibledesign.co>
2024-11-17 16:24:51 -0500

    Add component project-create

---
+++ src/app/features/projects/pages/project-create/project-create.component.ts
@@ -0,0 +1,45 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormArray, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-project-create',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './project-create.component.html',
+  styleUrl: './project-create.component.scss'
+})
+export class ProjectCreateComponent {
+  public formFamilyCore: FormGroup = new FormGroup({});
+  private fb = inject(FormBuilder);
+
+  ngOnInit(): void {
+    this.initFormFamilyCore();
+
+  }
+
+  initFormFamilyCore(): void {
+    this.formFamilyCore = this.fb.group({
+      nombre: ['', [Validators.required]],
+      categoria: ['', [Validators.required]],
+      fechaInicio: ['', [Validators.required]],
+      fechaFin: ['', [Validators.required]],
+      estado: ['', [Validators.required]],
+    });
+  }
+
+  // Referencia cada campo que se crea en el html
+  getCtrl(key: string, form: FormGroup): FormArray {
+    return form.get(key) as FormArray;
+  }
+
+  onSubmit() {
+    if(this.formFamilyCore.valid){
+      console.log("Form Family Core");
+      console.log(this.formFamilyCore.value);
+	  }else{
+		  this.formFamilyCore.markAllAsTouched();
+	  }
+  }
+}
--- src/app/features/projects/projects.routes.ts
+++ src/app/features/projects/projects.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from "@angular/router";
 import { ProjectsListComponent } from "./pages/projects-list/projects-list.component";
-
+import { ProjectCreateComponent } from "./pages/project-create/project-create.component";
 export const PROJECTS_ROUTES: Routes = [
   {
     path: '',
     component: ProjectsListComponent
+  },
+  {
+    path: 'create',
+    component: ProjectCreateComponent
   }
 ]

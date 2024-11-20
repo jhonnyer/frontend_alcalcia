baf280f53c80078f08544ee3aeb2e75b3ecb27ed
José Luis Zúñiga <jose@tangibledesign.co>
2024-11-20 17:49:40 -0500

    Partial change in register auth

--- src/app/features/auth/pages/register/register.component.ts
+++ src/app/features/auth/pages/register/register.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormArray, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  public formFamilyCore: FormGroup = new FormGroup({});
+  private fb = inject(FormBuilder);
 
+  ngOnInit(): void {
+    this.initFormFamilyCore();
+
+  }
+
+  initFormFamilyCore(): void {
+    this.formFamilyCore = this.fb.group({
+      primerNombre: ['', [Validators.required]],
+      segundoNombre: ['', [Validators.required]],
+      primerApellido: ['', [Validators.required]],
+      segundoApellido: ['', [Validators.required]],
+      tipoDocumento: ['cc', [Validators.required]],
+      numeroIdentificacion: ['', [Validators.required]],
+      area: ['', [Validators.required]],
+      cargo: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      telefono: ['', [Validators.required]],
+      estado: [false, [Validators.required]],
+    });
+  }
+
+  // Referencia cada campo que se crea en el html
+  getCtrl(key: string, form: FormGroup): FormArray {
+    return form.get(key) as FormArray;
+  }
+
+  onSubmit() {
+    if(this.formFamilyCore.valid){
+      console.log("Form Family Core");
+      console.log(this.formFamilyCore.value);
+	  }else{
+		  this.formFamilyCore.markAllAsTouched();
+	  }
+  }
 }

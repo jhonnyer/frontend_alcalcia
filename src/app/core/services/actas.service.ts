fccafc5019cb45ab62188530cd1e2acc2558b19b
José Luis Zúñiga <jose@tangibledesign.co>
2024-11-23 22:05:21 -0500

    Add actas services

---
+++ src/app/core/services/actas.service.ts
@@ -0,0 +1,33 @@
+import { inject, Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { beneficiaryList } from '../data/beneficiary.data';
+import { IActa } from '../models/acta.model';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { PaginatedResponse } from '../models/pagination.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ActasService {
+  private readonly URL = environment.URL_API;
+  private http = inject(HttpClient);
+
+  getAll(): Observable<IActa[]> {
+    return this.http.get<IActa[]>(`${this.URL}/actas/list`).pipe(
+      tap(actas => {
+        actas.forEach(acta => {
+          // acta.fechaCreacion = this.formatDate(acta.fechaCreacion);
+          // acta.fechaEntrega = this.formatDate(acta.fechaEntrega);
+          acta.beneficiario.fechaNacimiento = this.formatDate(acta.beneficiario.fechaNacimiento);
+        });
+      })
+    );
+  }
+
+  private formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    return date.toISOString().split('T')[0]; // Extraemos solo la parte de la fecha
+  }
+
+}

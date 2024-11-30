213973ff406f567974e0b2d0aa694ee53dc592d5
jose <joseluis836@hotmail.com>
2024-11-30 14:03:50 -0500

    Add beneficiarioProyecto.service

--- src/app/core/models/beneficiarioProyecto.model.ts
+++ src/app/core/models/beneficiarioProyecto.model.ts
@@ -2,11 +2,13 @@ import { IBeneficiario } from "./beneficiary.models"
 import { IProyecto } from "./proyecto.model"
 
 export interface IBeneficiarioProyecto     {
-  "idBeneficiarioProyecto": 1;
-  "beneficiario": IBeneficiario;
-  "proyecto": IProyecto;
-  "esBeneficiarioActivo": boolean;
-  "fechaInicio": string;
-  "fechaFin": string | null;
-  "observaciones": string
+  idBeneficiarioProyecto: number;
+  idBeneficiario: number;
+  idProyecto: number;
+  esBeneficiarioActivo: boolean;
+  fechaInicio: string;
+  fechaFin: string | null;
+  observaciones: string;
 }
+
+
---
+++ src/app/core/services/beneficiarioProyecto.service.ts
@@ -0,0 +1,71 @@
+import { inject, Injectable } from '@angular/core';
+import { IBeneficiarioProyecto } from '../models/beneficiarioProyecto.model';
+import { environment } from '../../../environments/environment';
+import { Observable, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { PaginatedResponse } from '../models/pagination.model';
+import { checkToken } from '../interceptors/token-interceptor.interceptor';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BeneficiarioProyectoService {
+
+  private readonly URL = environment.URL_API;
+  private http = inject(HttpClient);
+
+  getAll(): Observable<IBeneficiarioProyecto[]> {
+    return this.http.get<IBeneficiarioProyecto[]>(`${this.URL}/beneficiarios-proyectos/list`, { context: checkToken() }).pipe(
+      tap(beneficiarios => {
+        beneficiarios.forEach(beneficiario => {
+          if(beneficiario.fechaFin){
+            beneficiario.fechaFin = this.formatDate(beneficiario.fechaFin);
+          }
+          beneficiario.fechaInicio = this.formatDate(beneficiario.fechaInicio);
+        });
+      })
+    );
+  }
+
+  getBeneficiarioByIdProyecto(idProyecto: string): Observable<IBeneficiarioProyecto> {
+    return this.http.get<IBeneficiarioProyecto>(`${this.URL}/beneficiarios-proyectos/${idProyecto}`, { context: checkToken() }).pipe(
+      tap(beneficiario => {
+        if(beneficiario.fechaFin){
+          beneficiario.fechaFin = this.formatDate(beneficiario.fechaFin);
+        }
+        beneficiario.fechaInicio = this.formatDate(beneficiario.fechaInicio);
+      })
+    );
+  }
+
+  post(beneficiarioProyecto: Partial<IBeneficiarioProyecto>): Observable<IBeneficiarioProyecto> {
+    return this.http.post<IBeneficiarioProyecto>(`${this.URL}/beneficiarios-proyectos`, beneficiarioProyecto ,{ context: checkToken() }).pipe(
+      tap(beneficiario => {
+        if(beneficiario.fechaFin){
+          beneficiario.fechaFin = this.formatDate(beneficiario.fechaFin);
+        }
+        beneficiario.fechaInicio = this.formatDate(beneficiario.fechaInicio);
+      })
+    );
+  }
+
+  asignarBeneficiarioAProyecto(proyectoId: string,beneficiarioProyecto: Partial<IBeneficiarioProyecto>): Observable<IBeneficiarioProyecto> {
+    return this.http.put<IBeneficiarioProyecto>(`${this.URL}/beneficiarios-proyectos/${proyectoId}`, beneficiarioProyecto, { context: checkToken() }).pipe(
+      tap(beneficiario => {
+        if(beneficiario.fechaFin){
+          beneficiario.fechaFin = this.formatDate(beneficiario.fechaFin);
+        }
+        beneficiario.fechaInicio = this.formatDate(beneficiario.fechaInicio);
+      })
+    );
+  }
+
+  deleteById(id: string) {
+    this.http.delete(`${this.URL}/beneficiarios-proyectos/${id}`, { context: checkToken() });
+  }
+
+  private formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    return date.toISOString().split('T')[0]; // Extraemos solo la parte de la fecha
+  }
+}

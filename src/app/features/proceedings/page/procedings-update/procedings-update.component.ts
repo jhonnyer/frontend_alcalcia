f38e022686e030e9eec9c940514696da7f8b61a0
jose <joseluis836@hotmail.com>
2024-12-06 21:36:12 -0500

    Add procedings update

---
+++ src/app/features/proceedings/page/procedings-update/procedings-update.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-procedings-update',
+  standalone: true,
+  imports: [],
+  styles: ``,
+  templateUrl: './procedings-update.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProcedingsUpdateComponent { }

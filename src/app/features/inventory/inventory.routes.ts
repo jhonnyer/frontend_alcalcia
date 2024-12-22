716fea93558e0940084f9f4a7feb11cb6be79d1f
jose <joseluis836@hotmail.com>
2024-12-21 23:20:43 -0500

    Delete componentes legacy

--- src/app/features/inventory/inventory.routes.ts
+++ src/app/features/inventory/inventory.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from "@angular/router";
-import { InventoryDetailComponent } from "./pages/inventory-detail/inventory-detail.component";
 import { InventoryCreateComponent } from "./pages/inventory-create/inventory-create.component";
-import { InventoryFormComponent } from "./pages/inventory-form/inventory-form.component";
 import { InventoryListComponent } from "./pages/inventory-list/inventory-list.component";
 import { InventoryUpdateComponent } from "./pages/inventory-update/inventory-update.component";
 
@@ -17,18 +15,5 @@ export const INVENTORY_ROUTES: Routes = [
   {
     path: 'update/:id',
     component: InventoryUpdateComponent
-  },
-
-  {
-    path: 'detail',
-    component: InventoryDetailComponent
-  },
-  {
-    path: 'input',
-    component: InventoryFormComponent
-  },
-  {
-    path: 'output',
-    component: InventoryFormComponent
   }
 ]

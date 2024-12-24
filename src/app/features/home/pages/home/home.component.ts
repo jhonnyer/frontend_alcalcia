f73ca861a1eaf9ba4a4305d0526268c1473c6975
jose <joseluis836@hotmail.com>
2024-12-23 21:36:29 -0500

    Add navigate in home

--- src/app/features/home/pages/home/home.component.ts
+++ src/app/features/home/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { PageTitleService } from '../../../../core/services/pageTitle.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })

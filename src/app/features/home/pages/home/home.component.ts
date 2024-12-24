f66982ac4a10934c5d4a18f9118ba5417136d6e1
jose <joseluis836@hotmail.com>
2024-12-23 21:55:16 -0500

    Se agregan botones para reporte

--- src/app/features/home/pages/home/home.component.ts
+++ src/app/features/home/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { PageTitleService } from '../../../../core/services/pageTitle.service';
+import { ActasService } from '../../../../core/services/actas.service';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -11,8 +12,35 @@ import { RouterLink } from '@angular/router';
 })
 export class HomeComponent implements OnInit{
   private pageTitleService = inject(PageTitleService);
+  private actasService = inject(ActasService);
 
   ngOnInit(): void {
     this.pageTitleService.setCurrentPage('Home');
+  }
+
+  downloadExcel() {
+    this.actasService.getExcelActas().subscribe({
+      next: (blob: Blob) => {
+        // Crear URL del blob
+        const url = window.URL.createObjectURL(blob);
+
+        // Crear elemento a temporal
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'Actas.xlsx';
+
+        // Simular click para descargar
+        document.body.appendChild(link);
+        link.click();
+
+        // Limpieza
+        document.body.removeChild(link);
+        window.URL.revokeObjectURL(url);
+      },
+      error: error => {
+        console.error('Error al descargar el Excel:', error);
+        alert('Error al descargar el archivo Excel');
+      }
+    });
   }
 }

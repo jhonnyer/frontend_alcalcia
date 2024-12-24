8b52b654a5699b727b22e00d2bf34e3498e346a4
jose <joseluis836@hotmail.com>
2024-12-24 09:12:49 -0500

    Se elimina SSR

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { tokenInterceptor } from './core/interceptors/token-interceptor.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
-    provideClientHydration(),
     provideHttpClient(withFetch()),
     provideHttpClient(withInterceptors([tokenInterceptor]))
   ]

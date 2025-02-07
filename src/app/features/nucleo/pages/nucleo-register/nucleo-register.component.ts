f724d2d47dafc8491dcc9762a9087be02071a791
jhonnyer <jhonnyerg@unicauca.edu.co>
2025-02-07 12:46:59 -0500

    Ajuste creación nucleo asignacion barrio

--- src/app/features/nucleo/pages/nucleo-register/nucleo-register.component.ts
+++ src/app/features/nucleo/pages/nucleo-register/nucleo-register.component.ts
@@ -153,7 +153,7 @@ export class NucleoRegisterComponent implements OnDestroy {
   onSubmit() {
     if(this.formFamilyCore.valid){
       const zona = Number(this.formFamilyCore.get("idZonaFk")?.value);
-      const barrio = Number(this.formFamilyCore.get("idZonaFk")?.value);
+      const barrio = Number(this.formFamilyCore.get("idBarrioFk")?.value);
 
       this.formFamilyCore.patchValue({
         numeroIntegrantes: null,

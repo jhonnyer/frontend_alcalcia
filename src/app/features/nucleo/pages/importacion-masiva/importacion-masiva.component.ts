ce130d68d805f04e4f5f9b8d9c4f35eee332e2da
jhonnyer <jhonnyerg@unicauca.edu.co>
2026-09-24 15:04:51 -0500

    carga masiva

--- src/app/features/nucleo/pages/importacion-masiva/importacion-masiva.component.ts
+++ src/app/features/nucleo/pages/importacion-masiva/importacion-masiva.component.ts
@@ -47,10 +47,11 @@ export class ImportacionMasivaComponent {
     if (!fila.beneficiarioPrimerNombre?.trim()) errores.push('beneficiario_primer_nombre');
     if (!fila.beneficiarioPrimerApellido?.trim()) errores.push('beneficiario_primer_apellido');
     if (!['M', 'F'].includes(fila.sexo?.trim().toUpperCase())) errores.push('sexo');
-    if (fila.etnia?.trim() && !['C', 'I', 'A', 'N', 'E', 'M'].includes(fila.etnia.trim().toUpperCase())) errores.push('etnia');
-    if (fila.victimaConflicto?.trim() && !['SI', 'NO'].includes(fila.victimaConflicto.trim().toUpperCase())) errores.push('victima_conflicto');
-    if (fila.esVivo?.trim() && !['SI', 'NO'].includes(fila.esVivo.trim().toUpperCase())) errores.push('es_vivo');
-    if (fila.discapacidad?.trim() && !['SI', 'NO'].includes(fila.discapacidad.trim().toUpperCase())) errores.push('discapacidad');
+    if (!fila.genero?.trim()) errores.push('genero');
+    if (!fila.etnia?.trim() || !['C', 'I', 'A', 'N', 'E', 'M'].includes(fila.etnia.trim().toUpperCase())) errores.push('etnia');
+    if (!fila.victimaConflicto?.trim() || !['SI', 'NO'].includes(fila.victimaConflicto.trim().toUpperCase())) errores.push('victima_conflicto');
+    if (!fila.esVivo?.trim() || !['SI', 'NO'].includes(fila.esVivo.trim().toUpperCase())) errores.push('es_vivo');
+    if (!fila.discapacidad?.trim() || !['SI', 'NO'].includes(fila.discapacidad.trim().toUpperCase())) errores.push('discapacidad');
     if (!fila.tipoDocumento?.trim()) errores.push('tipo_documento');
     if (!['CC', 'TI', 'CE', 'NIT', 'PPT', 'RC'].includes(fila.tipoDocumento?.trim().toUpperCase())) errores.push('tipo_documento');
     if (!fila.numeroDocumento?.trim()) errores.push('numero_documento');
